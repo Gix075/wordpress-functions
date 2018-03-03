a79f1d16a11dc6e2804ef76eaf8c5939707da4b9
Gix075 <gildo.giuliani@gmail.com>
2018-03-03 16:45:16 +0100

    Update custom_gallery.php

--- custom_gallery.php
+++ custom_gallery.php
@@ -12,7 +12,7 @@ add_filter('post_gallery','customFormatGallery',10,2);
 function customFormatGallery($string,$attr){
 
     $posts = get_posts(array('include' => $attr['ids'],'post_type' => 'attachment'));
-    $output = '<div class="WPTheme__gallery"><div class="row mhTheme__chocolat chocolat-parent">';
+    $output = '<div class="WPTheme__gallery"><div class="row">';
     $columns = (isset($attr['columns'])) ? $attr['columns'] : "3";
     
     switch ($attr['columns']) {
@@ -27,12 +27,16 @@ function customFormatGallery($string,$attr){
             break;
     }
     
-    //printObject($attr);
+
     foreach($posts as $imagePost){
         $thumbnail = wp_get_attachment_image_src($imagePost->ID, $attr['size']);
         $image = wp_get_attachment_image_src($imagePost->ID, 'e');
-        $output .= '<a href="' . $image[0] . '" title="" class="WPTheme__gallery-item ' . $columns_class . '">';
-        $output .= '    <img src="' . $thumbnail[0] . '" alt="" title="">';
+        $title = $imagePost->post_excerpt;
+        $alt = get_post_meta($attachment->ID, '_wp_attachment_image_alt', true);
+        $alt = ($alt != "") ? $alt : $title;
+        $output .= '<a href="' . $image[0] . '" title="' . $title . '" class="WPTheme__gallery-item ' . $columns_class . '">';
+        $output .= '    <img class="WPTheme__gallery-item-image" src="' . $thumbnail[0] . '" alt="' . $alt . '" title="' . $title . '">';
+        $output .= '    <span class="class="WPTheme__gallery-item-over"><span>' . $title . '</span></span>';
         $output .= '</a>';
     }
 
@@ -40,4 +44,3 @@ function customFormatGallery($string,$attr){
 
     return $output;
 }
-
