3872c93844e411d59269aeb03c1b4f37f61a0473
Gix075 <gildo.giuliani@gmail.com>
2020-02-27 08:49:45 +0100

    Update terms-list.php

--- terms-list.php
+++ terms-list.php
@@ -1,13 +1,15 @@
 <?php
 
-
+// Show Hierarchical categories ordered by ID
 
 function custom_terms_list($options) {
 
     $options_default = array(
         "show_taxonomy_label" => false,
-        "show_terms_as_links => true
+        "show_terms_as_links" => true,
+        "separator" => ""
     );
+    $settings = array_replace_recursive($options_default, $options);
     $markup = '';
     global $post;
 
@@ -15,20 +17,33 @@ function custom_terms_list($options) {
 
     foreach ( $taxonomies as $taxonomy ) {
         $taxonomy = get_taxonomy( $taxonomy );
-        //print_r($taxonomy);
+
         if ( $taxonomy->query_var && $taxonomy->hierarchical ) {
 
             $markup .= '<div class="entry-categories">';
-                $markup .= '<h6>' . $taxonomy->labels->name . '</h6>';
+
+                if($settings['show_taxonomy_label'] === true) $markup .= '<h6>' . $taxonomy->labels->name . '</h6>';
 
                 $terms = wp_get_post_terms( $post->ID, $taxonomy->name, array( 'orderby' => 'term_id' ) );
-                foreach ( $terms as $term ) {
+
+                $i = 0;
+                foreach ( $terms as $key => $term ) {
                     //print_r($term);
-                    $link = get_term_link($term->term_id,$taxonomy->name);
-                    $markup .= '<a href="'.$link.'">'.$term->name.'</a>';
+
+                    $separator = ( !empty($settings['separator']) && $i != 0) ? $settings['separator'] : "";
+                    if( $settings['show_terms_as_links'] === true ) {
+                        $link = get_term_link($term->term_id,$taxonomy->name);
+                        $markup .= $separator.'<a href="'.$link.'">'.$term->name.'</a>';
+                    }else{
+                        $markup .= $separator."<span>".$term->name."</span>";
+                    }
+                    $i++;
 
                 }
-            $out .= '</div>';
+
+            $markup .= '</div>';
         }
     }
+
+    return $markup;
 }
